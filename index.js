8391a8768a7fca1bbf5fa79499903a68411cc454
mehedihasansufi <hasanmehedi205324@gmail.com>
2026-09-09 16:36:20 +0600

    home page problem added

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ fetch('./common_style/header.html')
     document.getElementById('header-placeholder').innerHTML=data;
 })
 
+fetch('./home_page/problem_card.html')
+.then(response=>response.text())
+.then(data=>{
+    document.getElementById('problem-card-placeholder').innerHTML=data;
+})
