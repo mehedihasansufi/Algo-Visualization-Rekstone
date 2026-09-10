d9b6f43857f93fceb62b64e9be98894e3f2dbce4
mehedihasansufi <hasanmehedi205324@gmail.com>
2026-09-10 21:49:20 +0600

    added the grid of one probelm

--- problem_page/problem.js
+++ problem_page/problem.js
@@ -4,3 +4,23 @@ fetch('../common_style/header.html')
 .then(data=>{
     document.getElementById('header-placeholder').innerHTML=data;
 })
+
+// leetcode problem added
+
+fetch('./leetcode_problem.html')
+.then(response=>response.text())
+.then(data=>{
+    document.getElementsByClassName("grid-leetcode-problem-list")[0].innerHTML=data;
+})
+const grid =document.getElementById('grid');
+const list=document.getElementById('list');
+
+grid.addEventListener('click',()=>{
+    grid.classList.add("grid-list-active")
+    list.classList.remove('grid-list-active')
+})
+
+list.addEventListener('click',()=>{
+    list.classList.add("grid-list-active");
+    grid.classList.remove('grid-list-active')
+})
